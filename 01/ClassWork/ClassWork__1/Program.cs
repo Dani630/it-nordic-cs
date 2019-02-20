0e2c2703758e16f4c1bb4764527e0c006a5d70ec
Daniil <daniplayerpro@gmail.com>
2019-02-20 21:50:46 +0300

    Lesson 1 class work added

---
+++ 01/ClassWork/ClassWork__1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+namespace ClassWork__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Привет {name}");
+            Console.ReadKey();
+        }
+    }
+}
