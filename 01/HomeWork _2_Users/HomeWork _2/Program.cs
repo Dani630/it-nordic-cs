94724dffb143b7a10934f6204f75499350979c15
Dani630 <daniplayerpro@gmail.com>
2019-03-03 19:55:11 +0300

    HomeWork_2_Users

---
+++ 01/HomeWork _2_Users/HomeWork _2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HomeWork__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = new string[5];
+            int[] age = new int[5];
+            for (int i = 0; i < name.Length; i++)
+            {
+                Green($"\nПользователь {i+1}.");
+                Yellow("\nВведите имя пользователя : ");
+                name[i] = Console.ReadLine();
+                Yellow($"Введите возвраст пользователя : ");
+                age[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+            Yellow("===============");
+            for (int i = 0; i < name.Length; i++)
+            {
+                Yellow("===============");
+                White($"\nПользователь {i + 1}.\nИмя : {name[i]}\nВозвраст : {age[i]}");
+                Yellow("===============");
+            }
+            Yellow("===============");
+        }
+        static void Yellow (string i)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(i);
+            Console.ResetColor();
+        }
+        static void Green (string i)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(i);
+            Console.ResetColor();
+        }
+        static void White (string i)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(i);
+            Console.ResetColor();
+        }
+    }
+}
