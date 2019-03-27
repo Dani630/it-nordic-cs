0cbc2f0f393d35b1bfc2831b9612497ece024acf
Daniil <daniplayerpro@gmail.com>
2019-03-27 21:26:36 +0300

    ClassWork_11

---
+++ 01/ClassWork_11/ClassWork_11/Pet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassWork_11
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public DateTime DateOfBirth;
+
+		public int Age 
+		{
+			get
+			{
+				TimeSpan age = DateTimeOffset.UtcNow.Subtract(DateOfBirth);
+				return Convert.ToInt32(Math.Floor(age.TotalDays / 365.242));
+			}
+		}
+
+		public string Description
+		{
+			get
+			{
+				return $"{Name} is a {Kind} ({Sex}) of {Age} years old.";
+			}
+		}
+
+		public string ShortDicription
+		{
+			get
+			{
+				return $"Nmae : {Name} , Age : {Age}";
+			}
+		}
+
+		public void WhiteDescription (bool showFullDescription)
+		{
+			/*
+			if (showFullDescription)
+				Console.WriteLine(Description);
+			else
+				Console.WriteLine(ShortDicription);
+				*/
+			Console.WriteLine(showFullDescription ? Description : ShortDicription);
+		}
+		
+	}
+}
---
+++ 01/ClassWork_11/ClassWork_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassWork_11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet p1 = new Pet();
+			p1.Name = "Andrei";
+			p1.Kind = "Dog";
+			p1.Sex = 'M';
+		//	p1.DateOfBirth = DateTimeOffset.Parse("2013-03-03");
+			//	p1.Age = 36;
+			Pet p2 = new Pet
+			{
+				Name = "Ilya ",
+				Kind = "Cat",
+				Sex = 'M',
+				//	Age = 18
+			};
+			p1.WhiteDescription(true);
+			p2.WhiteDescription(false);
+
+		}
+	}
+}
