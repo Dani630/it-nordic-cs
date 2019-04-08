2e02f83e5acb8ef3f47f246e9097b3d690f7fe91
Daniil <daniplayerpro@gmail.com>
2019-04-08 21:28:55 +0300

    ClassWork_14

---
+++ 01/ClassWork_14/ClassWork_14/ErrorList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWork_14
+{
+	class ErrorsList : IDisposable , IEnumerable<string>
+	{
+		public  string Category { get; private set; }
+
+		private List<string> _errors { get; set; }
+
+		public static string OutputPrefixFormat { get; set; }
+
+		
+		ErrorsList()
+		{
+			OutputPrefixFormat = Convert.ToString(DateTimeOffset.Now);
+		//	OutputPrefixFormat = /*Convert.ToString(*/new DateTimeOffset(04 / 7 / 2019);
+		}
+
+		public ErrorsList (string catrgory)
+		{
+			Category = catrgory;
+			_errors = new List<string>();
+		}
+
+		public void WriteToConsole()
+		{
+			foreach (var _er in _errors)
+			{
+				Console.WriteLine($"{_er} : {DateTime.Now.ToString(OutputPrefixFormat)}\n");
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_errors != null)
+			{
+				_errors.Clear();
+				_errors = null;
+			}
+		}
+
+		public void Add (string errorMessage)
+		{
+			_errors.Add(errorMessage);
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return _errors.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return _errors.GetEnumerator();
+		}
+	}
+}
---
+++ 01/ClassWork_14/ClassWork_14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassWork_14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{			
+			using (var errorsList = new ErrorsList("Errors"))
+			{
+				errorsList.Add("1");
+				errorsList.Add("2");
+
+				foreach (var write in errorsList)
+				{
+					Console.WriteLine($"{errorsList.Category} : {write}");
+				}
+
+				errorsList.WriteToConsole();
+			}
+			
+		}
+	}
+}
