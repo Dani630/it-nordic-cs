e9fcd6ee038a75d003bcac82a8f5782520bf50df
Dani630 <daniplayerpro@gmail.com>
2019-05-13 17:07:49 +0300

    ClassWork_22

---
+++ 01/ClassWork_22/ClassWork_22/Moldels/CityDeleteModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L22_C02_empty_asp_net_core_app_final.Moldels
+{
+    public class CityDeleteModel
+    {
+        public int Id { get; set; }
+    }
+}
